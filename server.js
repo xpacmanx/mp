598e626a57073c6050d8c64b7335f94eb679b19b
Jack Pack <ipacmanx@gmail.com>
2023-03-23 11:50:31 +0000

    fix server

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require('express')
 const serveStatic = require("serve-static")
 var history = require('connect-history-api-fallback')
-const path = require('path');
+const path = require('path')
 
-app = express();
+app = express()
 
 app.use(history({
 	verbose: true
 }))
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-const port = process.env.PORT || 3000;
-app.listen(port);
+const port = process.env.PORT || 3000
+app.listen(port)
