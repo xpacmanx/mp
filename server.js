6046421636e442c641bd9e7af84d0aa3d6ed0274
Jack Pack <ipacmanx@gmail.com>
2023-03-23 10:38:43 +0000

    update history

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const serveStatic = require("serve-static")
+var history = require('connect-history-api-fallback')
 const path = require('path');
 
 app = express();
 
-// Use a fallback for non-root routes (required for Vue router)
-//   NOTE: History fallback must be "used" before the static serving middleware!
 app.use(history({
-    // OPTIONAL: Includes more verbose logging
     verbose: true
 }))
 
