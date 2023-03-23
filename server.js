fe60c37217150c48eb57e515ee78d1391abf23c2
Jack Pack <ipacmanx@gmail.com>
2023-03-23 10:33:19 +0000

    fix server

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const serveStatic = require("serve-static")
 const path = require('path');
 
 app = express();
+
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 3000;
