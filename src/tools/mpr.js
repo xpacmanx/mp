e4a20558a7d8f0bf6d3795e84dd1cfa0ec15f66e
Jack Pack <ipacmanx@gmail.com>
2023-03-28 09:26:57 +0000

    test envirments

--- src/tools/mpr.js
+++ src/tools/mpr.js
@@ -1,17 +1,21 @@
 import axios from 'axios'
 
+console.log(import.meta.env.VITE_API_CLIENT, import.meta.env.API_SECRET)
+
 function createRequest(config) {
-	let { method, data, url, query } = config;
+	let { method, data, url, query, headers } = config;
 	if (method == undefined) method = 'get';
 	let API_SERVER = 'https://robot.teslalasers.com/webhook';
 	if (import.meta.env.DEV) API_SERVER += '/test';
 	url = API_SERVER + url;
-	console.log(url);
+
+	// headers[]
 
 	return axios({
-	  method: method,
-	  url: url,
-	  data: data,
+		method: method,
+		url: url,
+		data: data,
+		headers: headers,
 		query: query
 	});
 }
