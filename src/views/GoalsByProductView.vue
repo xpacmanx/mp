6de7c486b1e316510e87245d4e71be3d8c1236b1
Jack Pack <ipacmanx@gmail.com>
2023-03-29 07:43:02 +0000

    add products

--- src/views/GoalsByProductView.vue
+++ src/views/GoalsByProductView.vue
@@ -5,9 +5,9 @@
     <div class="content-with-menu">
       <div class="top-menu">
         <h2>4. Goals by product</h2>
-        <select>
-          <option>Коледино</option>
-          <option>Санкт-Петербург</option>
+				<span v-if="!loaded">Загрузка товаров...</span>
+        <select v-if="loaded">
+          <option v-for="product in products" :key="product.id">[{{product.code}}] {{product.name}}</option>
         </select>
         <div>
           <button class="btn">Создать подсорт</button>
@@ -25,12 +25,33 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 import Menu from '@/components/navigation/Menu.vue'
 import Header from '@/components/navigation/Header.vue'
+import mpr from './../tools/mpr'
 
 export default {
   name: 'GoalsByProductView',
   components: {
     HelloWorld, Menu, Header
-  }
+  },
+	data(){
+		return {
+			loaded: false,
+			products: [],
+			goals: [],
+		}
+	},
+	mounted(){
+		mpr({
+			url: '/products/all',
+		}).then(response => {
+			for (const item of response.data.result) {
+				item.add = 0;
+				this.products.push(item);
+			}
+			this.loaded = true;
+		}).catch((error) => {
+	     console.log('error', error);
+	  });
+	},
 }
 </script>
 
--- src/views/GoalsByWarehouseView.vue
+++ src/views/GoalsByWarehouseView.vue
@@ -38,6 +38,7 @@ export default {
 		return {
 			loaded: false,
 			warehouses: [],
+			goals: [],
 		}
 	},
 	mounted(){
