17ccac9b8b0f22323e0073f13def8a960b418b31
xpacmanx <ipacmanx@gmail.com>
2023-06-19 07:13:11 +0000

    add host clientPort

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
-		host: true,
+		host: '0.0.0.0',
 		port: process.env.PORT || 3000,
 		hmr: {
 			host: '0.0.0.0',
 			port: 443,
+			clientPort: 443,
 			protocol: 'wss',
 		},
 	},
