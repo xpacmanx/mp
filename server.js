8f3ff2e3e8e12ca16f72830b9d159ef3818156bb
Jack Pak <ipacmanx@gmail.com>
2025-02-21 16:54:39 +0700

    remove basic auth

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 const express = require('express')
 const serveStatic = require("serve-static")
-const basicAuth = require('express-basic-auth')
-
 var history = require('connect-history-api-fallback')
 const path = require('path')
 
 app = express()
-
-app.use(basicAuth({
-	users: { 'admin': 'Nikola1856' },
-	challenge: true,
-	realm: 'Imb4T3st4pp',
-}))
 
 app.use(history({
 	verbose: true
