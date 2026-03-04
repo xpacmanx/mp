bc6680b1520827cb00d730da452bfdc3841419dd
Jack Pak <ipacmanx@gmail.com>
2026-03-04 18:08:11 +0700

    feat: Implement new features and updates within the `NewSupplyTaskView2` component.

--- src/views/NewSupplyTaskView2.vue
+++ src/views/NewSupplyTaskView2.vue
@@ -301,7 +301,15 @@
 										@click="suggest(position);">⬅️</a>
 									{{Math.floor(position.suggest)}}
 								</td>
-								<td>{{position.main_and_already_packed}}</td>
+								<td>
+									<div>{{position.main_and_already_packed}}</div>
+									<div v-if="position.suggestion && position.suggestion.moscow_reserved_amount > 0" class="mt-1">
+										<span class="inline-block bg-blue-100 text-blue-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 whitespace-nowrap cursor-help"
+											title="Зарезервировано для первой поставки в Москву">
+											Зарезервировано {{position.suggestion.moscow_reserved_amount}} шт.
+										</span>
+									</div>
+								</td>
 								<td :class="position.notice_flag == 1 ? 'red' : ''">{{position.notice_flag == 1 ? 'Да' :
 									''}}</td>
 								<td :class="position.main_and_already_packed-position.task < 0 ? 'red' : ''">
